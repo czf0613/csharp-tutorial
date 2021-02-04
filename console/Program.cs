16979f8105331b039df8fbf617ab9d7e81d6768e
Kevin Chen <1354016594@qq.com>
2021-02-05 00:19:52 +0800

    基本控制语句
    条件语句、循环语句，带迭代器的范围循环语句、Range与索引类型简单介绍

--- console/Program.cs
+++ console/Program.cs
@@ -6,20 +6,18 @@ namespace console
     {
         static void Main(string[] args)
         {
-            var a = "123456789";
-            var b = @"aa
-123456";
+            var array = new int[100];
+            for (var i = 0; i < 100; ++i)
+                array[i] = i;
 
-            //“我今天去了 xx，花了xx元，买了xx。”
-            var place = "超市";
-            var price = 10.0;
-            var thing = "衣服";
+            //切片
+            int[] slice = array[1..10];
+            foreach (var i in slice)
+            {
+                Console.WriteLine(i);
+            }
 
-            var c = "我今天去了" + place + "，" + "花了" + price + "元，买了" + thing + "。";
-
-            c = $"我今天去了{place}，花了{price}元，买了{thing}。";
-
-            Console.WriteLine(c);
+            Console.WriteLine(array[^1]);
         }
     }
 }
