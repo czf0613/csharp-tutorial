75699f8102288dba8fed7e349ea928a2c0261f47
Kevin Chen <1354016594@qq.com>
2021-02-05 00:02:24 +0800

    字符串操作
    逐字字符串、字符串内插

--- console/Program.cs
+++ console/Program.cs
@@ -6,9 +6,20 @@ namespace console
     {
         static void Main(string[] args)
         {
-            var a = Console.ReadLine();
-            Console.WriteLine(a);
-            Console.WriteLine("{0} {1}", 1, 2);
+            var a = "123456789";
+            var b = @"aa
+123456";
+
+            //“我今天去了 xx，花了xx元，买了xx。”
+            var place = "超市";
+            var price = 10.0;
+            var thing = "衣服";
+
+            var c = "我今天去了" + place + "，" + "花了" + price + "元，买了" + thing + "。";
+
+            c = $"我今天去了{place}，花了{price}元，买了{thing}。";
+
+            Console.WriteLine(c);
         }
     }
 }
